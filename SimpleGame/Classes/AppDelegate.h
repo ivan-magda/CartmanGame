dbab31c19e7458040cbf747589aadc74c56f33ae
Ivan Magda <imagda15@gmail.com>
2014-09-21 14:39:30 +1000

    Initial Commit

---
+++ SimpleGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  SimpleGame
+//
+//  Created by Ivan Magda on 21.09.14.
+//  Copyright Ivan Magda 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ SimpleGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SimpleGame
+//
+//  Created by Ivan Magda on 21.09.14.
+//  Copyright Ivan Magda 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
